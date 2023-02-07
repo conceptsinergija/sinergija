39347c0b29d8853d4407cbcfd62dfa1f36adcea1
Evgeniya <evgeniya@ultimatemovers.net>
2023-02-07 13:30:37 +0100

    added slider on meet our team

--- js/main.js
+++ js/main.js
@@ -95,6 +95,31 @@ import SmoothScroll from './smoothScroll.js'
     })
   }
 
+  if(document.querySelector('#meet-the-team')) {
+    var swiper = new Swiper('.swiper', {
+      spaceBetween: 30,
+      slidesPerView: 3,
+      loop: true,
+      direction: getDirection(),
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      on: {
+        resize: function () {
+          swiper.changeDirection(getDirection());
+        },
+      },
+    });
+
+    function getDirection() {
+      var windowWidth = window.innerWidth;
+      var direction = window.innerWidth <= 780 ? 'vertical' : 'horizontal';
+
+      return direction;
+    }
+};
+
 
   if(document.querySelector('#faq')) {
     const faqTitlesItems = document.querySelectorAll('.faq-category')
